64b28a01172f706b4914b53383ce09a12b16164d
hoarus <hoarus@gmail.com>
2021-12-19 09:47:10 +1000

    Add fields for Create Event form

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
   belongs_to :creator, class_name: "User"
+  
+  validates :title, length: { in: 3..16 }, presence: true
+  validates :location, length: { in: 3..90 }, presence: true
+  validates :date, presence: true
+  validates :creator_id, presence:true
 end
