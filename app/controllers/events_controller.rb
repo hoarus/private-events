acf886250c984bf24ddff89acab712069ddafc84
hoarus <hoarus@gmail.com>
2021-12-18 15:51:10 +1000

    Create Event Show view

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,73 @@
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+  
+  # GET /events 
   def index
     @events = Event.all
   end
+
+  # GET /events/1 or /events/1.json
+  def show
+    @event = Event.find(params[:id])
+  end 
+
+  # GET /events/new
+  def new
+    @event = current_user.events.build
+  end
+
+  # GET /events/1/edit
+  def edit
+  end
+
+  # POST /events or /events.json
+  def create
+    @event = current_user.events.build(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to @event, notice: "event was successfully created." }
+        format.json { render :show, status: :created, location: @event }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /events/1 or /events/1.json
+  def update
+    respond_to do |format|
+      if @event.update(event_params)
+        format.html { redirect_to @event, notice: "event was successfully updated." }
+        format.json { render :show, status: :ok, location: @event }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /events/1 or /events/1.json
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_url, notice: "event was successfully destroyed." }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_event
+      @event = event.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def event_params
+      params.require(:event).permit(:title, :location, :date, :creator_id)
+    end
 end
+
+
+
